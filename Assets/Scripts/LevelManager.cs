bd8cf29fec79a683650cddca79c300feb32a3d3c
William <willcallihan13@gmail.com>
2023-03-30 09:44:07 -0500

    added pause menu

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,9 +4,47 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
 
+    [SerializeField] private GameObject pauseScreen;
+
+    private bool paused;
+
 	private void Awake() {
-		//lock and hide mouse during gameplay
-		Cursor.visible = false;
-		Cursor.lockState = CursorLockMode.Locked;
+        Unpause();
 	}
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(paused) {
+                Unpause();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    //called by pause input
+    private void Pause() {
+        //set the pause flag
+        paused = true;
+        //freeze time
+        Time.timeScale = 0;
+        //show the pause screen
+        pauseScreen.SetActive(true);
+        //unlock and show mouse
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    //called by pause input and by the Resume button on PauseScreen
+    public void Unpause() {
+        //set time back to normal
+        Time.timeScale = 1;
+        //hide the pause screen
+        pauseScreen.SetActive(false);
+        //lock and hide mouse during gameplay
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        //set the pause flag
+        paused = false;
+    }
 }
