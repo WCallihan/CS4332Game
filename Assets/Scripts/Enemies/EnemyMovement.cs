ccff1296607fc1827a89899d8f72b35fb3f0d977
William <willcallihan13@gmail.com>
2023-05-06 14:16:28 -0500

    rooms 3 and 4 nav meshes, more enemy movement tweaks

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -5,8 +5,6 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour {
 
-    [SerializeField] private float stoppingDistance; //stopping distance from the player
-
 	private GameObject player;
     private EnemyShooting enemyShooting;
     private NavMeshAgent navAgent;
@@ -21,21 +19,16 @@ public class EnemyMovement : MonoBehaviour {
         playerSighted = false;
     }
 
-    void FixedUpdate() {
+    void Update() {
         //player is sighted if they come within weapon range
         if(!playerSighted && Vector3.Distance(transform.position, player.transform.position) <= weaponRange) {
             playerSighted = true;
             enemyShooting.PlayerSighted(player); //tells the shooter to start shooting
         }
 
-        //if the player has been seen and is not within a reasonable range, move towards them
-        if(playerSighted && Vector3.Distance(transform.position, player.transform.position) > stoppingDistance) {
+        //if the player has been seen, move towards them
+        if(playerSighted) {
             navAgent.destination = player.transform.position; //set the nav mesh agent destination to the player
-            navAgent.isStopped = false; //start moving again
-
-        //if the player is within a reasonable range, stop moving
-        } else {
-            navAgent.isStopped = true; //stop moving
         }
 
         //face the player if they are sighted and the enemy isn't moving
