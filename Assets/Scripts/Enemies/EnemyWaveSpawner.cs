43b7a2cd030f86bbb09344a0cd2a2a8741b3bf64
William <willcallihan13@gmail.com>
2023-05-07 11:05:10 -0500

    changed enemy spawning to random locations in each rom

--- Assets/Scripts/Enemies/EnemyWaveSpawner.cs
+++ Assets/Scripts/Enemies/EnemyWaveSpawner.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyWaveSpawner : MonoBehaviour {
 
-	[SerializeField] private GameObject[] enemyWave1;
-	[SerializeField] private GameObject[] enemyWave2;
+    [Header("Enemy Waves")]
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private int minEnemies;
+    [SerializeField] private int maxEnemies;
+
+    [Header("Spawning Bounds")]
+    [SerializeField] private float minX;
+    [SerializeField] private float maxX;
+    [SerializeField] private float minZ;
+    [SerializeField] private float maxZ;
 
 	private int currentWave;
 	private int totalWaveEnemies;
@@ -17,17 +26,30 @@ public class EnemyWaveSpawner : MonoBehaviour {
 	private void Awake() {
 		currentWave = 0;
 		waveEnemiesKilled = 0;
-		StartCoroutine(SpawnWaveDelay(enemyWave1));
+		StartCoroutine(SpawnWaveDelay());
 	}
 
-	private void SpawnNewWave(GameObject[] enemyWave) {
+	private void SpawnNewWave() {
 		currentWave++;
-		totalWaveEnemies = enemyWave.Length;
-		waveEnemiesKilled = 0;
-		foreach(var e in enemyWave) {
-			e.GetComponent<EnemyHealth>().EnemyDied += OnEnemyDeath;
-			e.SetActive(true);
-		}
+        totalWaveEnemies = UnityEngine.Random.Range(minEnemies, maxEnemies + 1); //get how many enemies will be spawned
+        waveEnemiesKilled = 0;
+
+        for(int i=0; i<totalWaveEnemies; i++) {
+            //find a random spawn position on the nav mesh
+            Vector3 spawnPos = Vector3.zero;
+            do {
+                float randX = UnityEngine.Random.Range(minX, maxX);
+                float randZ = UnityEngine.Random.Range(minZ, maxZ);
+                NavMeshHit hit;
+                if(NavMesh.SamplePosition(new(randX, 0, randZ), out hit, 1, NavMesh.AllAreas)) {
+                    spawnPos = hit.position;
+                }
+            } while(spawnPos == Vector3.zero);
+            //spawn the enemy and subscribe the function to its death event
+            var enemy = Instantiate(enemyPrefab, spawnPos + new Vector3(0f, 2f, 0f), enemyPrefab.transform.rotation);
+            enemy.GetComponent<EnemyHealth>().EnemyDied += OnEnemyDeath;
+        }
+        Debug.Log("Spawning " + totalWaveEnemies + " Enemies");
 	}
 
 	private void OnEnemyDeath() {
@@ -37,7 +59,7 @@ public class EnemyWaveSpawner : MonoBehaviour {
 		if(waveEnemiesKilled >= totalWaveEnemies) {
 			//since the wave is over, spawn the next one if there is one
 			if(currentWave == 1) {
-				StartCoroutine(SpawnWaveDelay(enemyWave2));
+				StartCoroutine(SpawnWaveDelay());
 			} else if(currentWave == 2) {
 				AllEnemiesDead?.Invoke();
 			} else {
@@ -47,8 +69,23 @@ public class EnemyWaveSpawner : MonoBehaviour {
 	}
 
 	//adds small delay between enemy waves
-	private IEnumerator SpawnWaveDelay(GameObject[] enemyWave) {
+	private IEnumerator SpawnWaveDelay() {
 		yield return new WaitForSeconds(2);
-		SpawnNewWave(enemyWave);
+		SpawnNewWave();
 	}
+
+    //draw a rectangle around the set spawning bounds
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        //define the corners
+        Vector3 topLeft = new(minX, 0, maxZ);
+        Vector3 topRight = new(maxX, 0, maxZ);
+        Vector3 bottomRight = new(maxX, 0, minZ);
+        Vector3 bottomLeft = new(minX, 0, minZ);
+        //draw the rectangle
+        Gizmos.DrawLine(topLeft, topRight);
+        Gizmos.DrawLine(topRight, bottomRight);
+        Gizmos.DrawLine(bottomRight, bottomLeft);
+        Gizmos.DrawLine(bottomLeft, topLeft);
+    }
 }
